193f1d46ebfc05ef2c71aa2a7244f84ac4139419
Jack CARBONERO <117528398+jack-carbonero-64@users.noreply.github.com>
2024-04-08 23:14:10 +0200

    Main window - Video and subtitles file selection

--- src/main_window.py
+++ src/main_window.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QLineEdit, QFileDialog
 from PyQt6.uic import loadUi
 from config import config
 import os
@@ -18,3 +18,21 @@ class MainWindow(QMainWindow):
 
         # Load the main window UI
         loadUi(os.path.join(config.UI_DIR, "main_window.ui"), self)
+
+        # Retrieve the useful UI objects
+        self.videoLineEdit = self.findChild(QLineEdit, "videoLineEdit")
+        self.subtitlesLineEdit = self.findChild(QLineEdit, "subtitlesLineEdit")
+
+    def onBrowsePushButtonClicked(self):
+        """
+        Display a file dialog to select a video or a subtitles file depending on the clicked button
+        """
+
+        senderName = self.sender().objectName()
+        filePath = QFileDialog.getOpenFileName(self)[0]
+
+        if "videoBrowse" in senderName:
+            self.videoLineEdit.setText(filePath)
+
+        elif "subtitlesBrowse" in senderName:
+            self.subtitlesLineEdit.setText(filePath)
